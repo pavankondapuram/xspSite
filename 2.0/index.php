879aef612f6630459dccf2ac1aa85adfa0858582
Pavan Kondapuram <pavan@graphicana.co.in>
2012-10-31 10:28:12 +0530

    new files for 2.0

---
+++ 2.0/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>XSP <Xtra Sensory Perception /></title>
+		<link href="images/favIcon.png" rel="shortcut icon">
+		<link rel="stylesheet" type="text/css" href="css/styles.css">
+	</head>
+	<body>
+		<nav>
+			<ul>
+				<li>Overview</li>
+				<li>Services</li>
+				<li>Process</li>
+				<li>Gallery</li>
+				<li>Contact us</li>
+			</ul>
+		</nav>
+		<script src="http://ajax.googleapis.com/ajax/libs/dojo/1.8.1/dojo/dojo.js"></script>
+		<script src="js/glo.js" type="text/javascript"></script>
+	</body>
+</html>
