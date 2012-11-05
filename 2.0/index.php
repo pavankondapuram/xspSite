a6f25e0bf8fe67f2951973fe9f0e8f6a573abdc8
pavan <pavan@graphicana.co.in>
2012-11-05 17:47:40 +0530

    started with styles and content

--- 2.0/index.php
+++ 2.0/index.php
@@ -7,6 +7,10 @@
 		<link rel="stylesheet" type="text/css" href="css/styles.css">
 	</head>
 	<body>
+		<div id="header">
+			<div id="xsp"></div>
+			<div class="logo"></div>
+		</div>
 		<nav>
 			<ul>
 				<li>Overview</li>
@@ -16,6 +20,30 @@
 				<li>Contact us</li>
 			</ul>
 		</nav>
+		<div id="home">We are web architects</div>
+		<div id="overView">
+			<p>We are formulated to elite a desired solution to corporate and individual clients. We are leading Web 2.0 firm delivering highly creative, brand-driven and result focused web solution. The Internet and web technology prospect is forever evolving, the demand for high-impact experiences rises to new levels, as we constantly strive to achieve lasting impressions for our clients.</p>
+			<p>XSP was founded in mid 2011 to serve the web and mobile industry. We are ambitions to build applications and frameworks to address gaps in web and mobile application design and development. Read below to find about the two founders of XSP.</p>
+			<ul>
+				<li class="xsp">
+					<h2>About XSP</h2>
+					<p>XSP < XtraSensoryPerception ⁄> started as the joint operation between designer and developer. To encompass the proper user experience and functionality of any pretty things on the web, both designer and developer plays the major role to combine and build pretty things.</p>
+				</li>
+				<li class="attributes">
+					<h2>Our attribute</h2>
+					<p>we build pretty things for the web, no matter whether it’s phone or desktop. Our services are widely differentiated from creative services to full scale back end web application. We adroit to simulate your imagination in sophisticated bending of the rules and formulate a plan to meet your business requirement.</p>
+				</li>
+				<li class="process">
+					<h2>Way we carryout things</h2>
+					<p>We give greatest degree of Special thought for expression that gives importance to project architecture for the point at which the condition is most favourable for Web Design and Development project. We follow the methods to engage in an inquiry and we assure the satisfaction of our customers to the core.</p>
+				</li>
+			</ul>
+		</div>
+		<div id="services"></div>
+		<div id="process"></div>
+		<div id="gallery"></div>
+		<div id="contactUs"></div>
+		<footer></footer>
 		<script src="http://ajax.googleapis.com/ajax/libs/dojo/1.8.1/dojo/dojo.js"></script>
 		<script src="js/glo.js" type="text/javascript"></script>
 	</body>
