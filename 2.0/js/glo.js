9d744757ca3530b33f107e9a5bd2cd915fea1910
pavan <pavan@graphicana.co.in>
2012-11-09 18:14:50 +0530

    js small update

--- 2.0/js/glo.js
+++ 2.0/js/glo.js
@@ -1,8 +1,12 @@
-require(["dojo/window","dojo/query"],
-	function(win,query){
+require(["dojo/window","dojo/query","dojo/dom-style"],
+	function(win,query,domStyle){
 		var vs = win.getBox(),
-		wrapper = query(".wrapper");
+		wrapper = query(".wrapper"),
+		wrapperStyles = domStyle.get(wrapper[0]);
 		console.log(vs.h);
 		console.log(wrapper);
+		console.log(wrapperStyles);
+		dojo.style(wrapper[1],"min-height",vs.h+"px");
+		console.log(domStyle.get(wrapper[0]));
 	}
 );
