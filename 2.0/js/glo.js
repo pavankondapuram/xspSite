c068ac1d0ce2d1812de022eac059814b06a5059d
pavan <pavan@graphicana.co.in>
2012-11-19 18:26:24 +0530

    working copy

--- 2.0/js/glo.js
+++ 2.0/js/glo.js
@@ -10,71 +10,40 @@ require(["dojo/window",
 		"dojo/dom-class",
 		"dojo/_base/fx",
 		"dojo/domReady!"],
-
+		
 	function(win,query,domStyle,on,domClass,fx){
-		/*var vs = win.getBox(),
+		var vs = win.getBox(),
 		wrpHt = vs.h+"px",
 		ftPsn = (vs.h-24)+"px",
 		wrp = query(".wrapper"),
 		footer = query("footer"),
 		wrpStl = domStyle.get(wrp,"min-height"),
 		des = query("nav ul li"),
-		hmDv = query("#home");*/
-
-		/*console.log(vs.h);
+		hmDv = query("#home");
+		
+		console.log(vs.h);
 		wrp.forEach(function(wrp){
 			domStyle.set(wrp,"minHeight",wrpHt);
 		});
-
+		
 		domStyle.set(footer[0],"top",ftPsn);
 		
+		/*if(wrpHt>800){
+			domStyle.set(hmDv[0],backgroundPsition,"50% bottom")
+		};*/
+		
+		/*on(des,"click",function(){
+			alert(this);
+		})*/
 		on(des,"click",function(){
 			var inrCnt = this.innerHTML.toLowerCase().replace(/\s/g, '');
-
+			
 			//console.log(inrCnt);
 			//location.hash= "#"+inrCnt;
 			//win.scrollIntoView(inrCnt);
 			fx.anim(this, win.scrollIntoView(inrCnt), 300);
 			domClass.toggle(this, "selected");
-
-		});*/
-
-
-		function MyWindow() {
-			this.minHeight = null;
-		}
-
-		MyWindow.prototype.move = function () {
-			this.minHeight= win.getBox().h;
-		}
-
-		MyWindow.prototype.moveTo = function (position) {
-			var current = this.height;
-			this.height = new height;
-		}
-
-
-
-		var window = new MyWindow();
-		window.move();
-
-
-
-		var movinWin = {
-			windw:query(".wrapper"),
-			windwDim:win.getBox(),
-			ht:windwDim.h+"px",
-			ft:query("footer"),
-			ftPsn:(vs.h-24)+"px",
-			trgr:query("nav ul li"),
-		}
-
-		function movinWin(ht){
-
-			this.windw.forEach(function(){
-				domStyle.set(windw,"minHeight",ht);
-			})
-		}
-		
+			
+		});
 	}
 );
