8d18b62a66ecce084ce58ff20a4bb0f9a018ccf3
pavan <pavan@graphicana.co.in>
2012-11-26 17:49:30 +0530

    jq work in progress

--- 2.0/js/glo_jq.js
+++ 2.0/js/glo_jq.js
@@ -1,11 +1,27 @@
-function windw(resize,navigate){
+/*function windw(resize,navigate){
 	this.resize="null";
 	this.navigate="";
-}
+};
 
 windw.prototype.resize = function(){
 	var winMinHt = $("html").innerHTML(),
 		sectn = $(this);
 
 	sectn.css("min-height",winMinHt+"px");
-}
+};
+
+var home = new windw();*/
+
+$(function(){
+	$(".wrapper").css("min-height",$(window).innerHeight()+"px");
+
+	$("footer").css("top",($(window).innerHeight())-28+"px");
+
+	$("nav ul li").click(function(){
+		$("nav ul li").removeClass("selected");
+		$(this).addClass("selected");
+
+		$(window).scroll("#"+$(this).html().toLowerCase().replace(/\s/g, ''));
+		console.log($(this).html().toLowerCase().replace(/\s/g, ''));
+	})
+});
