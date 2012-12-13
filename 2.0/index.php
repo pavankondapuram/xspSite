b95599cf6578b81813cad08116d9cd7f71999806
Pavan Kondapuram <pavan@graphicana.co.in>
2012-12-13 23:22:36 +0530

    more content

--- 2.0/index.php
+++ 2.0/index.php
@@ -59,19 +59,42 @@
 		<div id="process">
 			<div class="wrapper">
 				<h2>Way we achieve things</h2>
+				<p>We approach and execute every project with acute professionalism. We follow the under mentioned methodology for proper preparation, development, and execution of a project to attain our goals. Here are the steps that we utilize during the execution of any project.</p>
+				<ul>
+					<li>
+						<h3>Instantiate</h3>
+						<p>In the beginning stage of chain of development, we work with you and your team to know the actual existence of concepts and abstraction of your objectives and goals. There is lot of communication between us and your team at this stage. Lets rare a share to bring those imaginations into the first stage of its existence.</p>
+					</li>
+					<li>
+						<h3>Design</h3>
+						<p>With the knowledge acquired through the instantiate stage we start the theoretical reviews and construct a plan with concept. At the end of this stage we will be having a clear picture on changing your imagination into real time, fully functional application.</p>
+					</li>
+					<li>
+						<h3>Foster</h3>
+						<p>This is where the attempt starts to build product in real. Your project goes from design phase till actually working module. In this stage all the plans and concepts from the earlier stage will turn into prototype.</p>
+					</li>
+					<li>
+						<h3>Launch</h3>
+						<p>The prototype from foster goes into different stages of testing. Once we are very sure about the product is running very smooth with a error free environment, the product is set to launch. It is the feeling of lively and cheerful joy, as the great effort to endure comes to the condition of bearing fruit and the project comes alive.</p>
+					</li>
+					<li>
+						<h3>Review</h3>
+						<p>Don’t guess launch is the final stage of all. Once the product is launched, the application is reviewed and future updates are set in consideration with regard to growth and advancement. We like come into begin and accomplish each project with ascent to rising.</p>
+					</li>
+				</ul>
 			</div>			
 		</div>
 		<div id="gallery">
 			<div class="wrapper">
 				<h2>Show off</h2>
-			</div>			
+			</div>
 		</div>
 		<div id="contactus">
 			<div class="wrapper">
 				<h2>Utilize us</h2>
 			</div>
 		</div>
-		<footer>This is the footer</footer>
+		<footer>© Copy Rights reserved - XSP Xtra Sensory Perception</footer>
 		<script src="js/dojo/dojo/dojo.js"></script>
 		<script src="js/fw/jQuery.js"></script>
 		<script src="js/fw/jqueryScrollTo.js"></script>
--- 2.0/js/glo_jq.js
+++ 2.0/js/glo_jq.js
@@ -19,9 +19,9 @@ var home = new windw();*/
 
 $(function(){
 	var pos = $(window).innerHeight();
-	$(".wrapper").css("min-height",pos/*-120+"px"*/);
+	$(".wrapper").css("min-height",pos);
 
-	$("footer").css("top",pos-28+"px");
+	$("footer").css("top",pos-26+"px");
 
 	$("nav ul li").click(function(){
 
@@ -33,199 +33,47 @@ $(function(){
 		//$("body,html").animate({scrollTop:($(this).index())*pos},300,"swing");
 		
 		//console.log(clicked);//$("body,html").scrollTo($(clicked),{duration:300});
-		$("body,html").scrollTo("#"+clicked, 800);
+		$("body,html").scrollTo("#"+clicked, 600);
 		
 	});
 
 	$(window).scroll(function(){
 		//var posss = ($("nav ul li").offset().top);
-		var posss = scrollTo();
-		console.log(posss);
+		//var posss = scrollTo();
+		//console.log(posss);
 	});
 });
 
 
-(function() {
-    var lastTime = 0;
-    var vendors = ['ms', 'moz', 'webkit', 'o'];
-    for(var x = 0; x < vendors.length && !window.requestAnimationFrame; ++x) {
-        window.requestAnimationFrame = window[vendors[x]+'RequestAnimationFrame'];
-        window.cancelAnimationFrame = window[vendors[x]+'CancelAnimationFrame'] 
-                                   || window[vendors[x]+'CancelRequestAnimationFrame'];
-    }
- 
-    if (!window.requestAnimationFrame)
-        window.requestAnimationFrame = function(callback, element) {
-            var currTime = new Date().getTime();
-            var timeToCall = Math.max(0, 16 - (currTime - lastTime));
-            var id = window.setTimeout(function() { callback(currTime + timeToCall); }, 
-              timeToCall);
-            lastTime = currTime + timeToCall;
-            return id;
-        };
- 
-    if (!window.cancelAnimationFrame)
-        window.cancelAnimationFrame = function(id) {
-            clearTimeout(id);
-        };
-}());
+//contact form validation
 
-var Nodes = {
-
-  // Settings
-  density: 6,
-  reactionSensitivity: 3,
-
-  points: [],
-  lines: [[], []],
-  mouse: { x: -1000, y: -1000, down: false },
-
-  animation: null,
-
-  canvas: null,
-  context: null,
-
-  init: function() {
-    // Set up the visual canvas 
-    this.canvas = document.getElementById( 'stripes' );
-    this.context = this.canvas.getContext( '2d' );
-    this.context.lineJoin = 'bevel';
-    this.canvas.width = window.innerWidth;
-    this.canvas.height = window.innerHeight;
-    this.canvas.style.display = 'block'
-
-    this.canvas.addEventListener('mousemove', this.mouseMove, false);
-    this.canvas.addEventListener('mousedown', this.mouseDown, false);
-    this.canvas.addEventListener('mouseup',   this.mouseUp,   false);
-    this.canvas.addEventListener('mouseout',  this.mouseOut,  false);
-
-    window.onresize = function(event) {
-      Nodes.canvas.width = window.innerWidth;
-      Nodes.canvas.height = Math.max(window.innerHeight, 670);
-      Nodes.onWindowResize();    
-    }
-
-    this.preparePoints();
-    this.draw();
-    
-  },
-
-  preparePoints: function() {
-
-    // Clear the current points
-    this.points = [];
-    this.lines = [[],[]];
-    
-    var width, height, i;
-    var center = window.innerHeight / 2;
-
-    for( i = -10; i < this.canvas.width + 10; i += this.density ) {
-
-      var line1 = {y: center - 17, originalY: center - 10, color: '#B1FB17'};
-      var line2 = {y: center - 17, originalY: center - 25, color: '#F52887'};
-
-      line1["x"] =  i;
-      line1["originalX"] = i;
-      
-      line2["x"] =  i;
-      line2["originalX"] = i;
-      
-      this.points.push(line1);
-      this.points.push(line2);
-      
-      this.lines[0].push(line1);
-      this.lines[1].push(line2);
-    }
-  },
-
-  updatePoints: function() {
-
-    var i, currentPoint, theta, distance;
-    
-    for (i = 0; i < this.points.length; i++ ){
-
-      currentPoint = this.points[i];
-
-      theta = Math.atan2( currentPoint.y - this.mouse.y, currentPoint.x - this.mouse.x);
-
-      if ( this.mouse.down ) {
-        distance = this.reactionSensitivity * 300 / Math.sqrt((this.mouse.x - currentPoint.x) * (this.mouse.x - currentPoint.x) +
-         (this.mouse.y - currentPoint.y) * (this.mouse.y - currentPoint.y));
-      } else {
-        distance = this.reactionSensitivity * 150 / Math.sqrt((this.mouse.x - currentPoint.x) * (this.mouse.x - currentPoint.x) +
-         (this.mouse.y - currentPoint.y) * (this.mouse.y - currentPoint.y));  
+$(document).ready(function(){
+    $("#myform").validate({
+      debug: false,
+      rules: {
+        name: "required",
+        contact_no: { 
+                required:true,
+                number:true
+        },
+        email: {
+          required: true,
+          email: true
+        },
+        message: "required"
+      },
+      messages: {
+        name: "Please let us know who you are",
+        contact_no: "Please provide your contact number",
+        email: "A valid email will help us get in touch with you",
+        message: "Pleasge enter a message",
+      },
+      submitHandler: function(form) {
+        // do other stuff for a valid form
+        $.post('admin/cu-core-functions.php', $("#myform").serialize(), function(data) {
+          $('#results').html(data);
+          document.getElementById('myform').reset();
+        });
       }
-      
-      currentPoint.x += Math.cos(theta) * distance + (currentPoint.originalX - currentPoint.x) * 0.07;
-      currentPoint.y += Math.sin(theta) * distance + (currentPoint.originalY - currentPoint.y) * 0.07;
-
-    }
-  },
-
-  drawPoints: function() {
-
-    var i, currentPoint;
-
-    for (i = 0; i < 2; i++) {
-      var line = this.lines[i];
-      this.context.beginPath();
-      this.context.lineJoin = 'round';
-      this.context.moveTo( line[0].x, line[0].y);
-      this.context.strokeStyle = line[0].color;
-      this.context.lineWidth = 10;
-      for (var j = 1; j < line.length - 2; j++) {
-        var point = line[j];
-
-        var xc = (point.x + line[j + 1].x) / 2;
-        var yc = (point.y + line[j + 1].y) / 2;
-
-        this.context.quadraticCurveTo(point.x, point.y, xc, yc);
-
-      }
-      this.context.stroke();
-      this.context.closePath();
-    }
-  },
-
-  draw: function() {
-    this.animation = requestAnimationFrame( function(){ Nodes.draw() } );
-
-    this.clear();
-    this.updatePoints();
-    this.drawPoints();
-
-  },
-
-  clear: function() {
-    this.canvas.width = this.canvas.width;
-  },
-
-  mouseDown: function( event ){
-    Nodes.mouse.down = true;
-  },
-
-  mouseUp: function( event ){
-    Nodes.mouse.down = false;
-  },
-  
-  mouseMove: function(event){
-    Nodes.mouse.x = event.pageX;
-    Nodes.mouse.y = event.pageY;
-  },
-  
-  mouseOut: function(event){
-    Nodes.mouse.x = -1000;
-    Nodes.mouse.y = -1000;
-    Nodes.mouse.down = false;
-  },
-
-  // Resize and redraw the canvas.
-  onWindowResize: function() {
-    cancelAnimationFrame( this.animation );
-    this.preparePoints();
-    this.draw();
-  }
-
-}
-    
-Nodes.init();
+    });
+  });
