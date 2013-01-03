407ff07e63d3e237e772935b1ce8c76ee9d80880
Pavan Kondapuram <pavan@graphicana.co.in>
2013-01-04 00:07:08 +0530

    some ascetics

--- 2.0/index.php
+++ 2.0/index.php
@@ -92,6 +92,21 @@
 		<div id="contactus">
 			<div class="wrapper">
 				<h2>Utilize us</h2>
+				<div class="needField" id="results"> </div>
+				<div class="unit">
+				  <form name="myform" id="myform" action="" method="POST">
+				    <input class="input" name="name" type="text" placeholder="Name" >
+				    <br/>
+				    <input class="input" name="contact_no" type="text" placeholder="Contact number">
+				    <br/>
+				    <input class="input" name="email" type="text" placeholder="eMail">
+				    <br/>
+				    <textarea class="input" placeholder="Message" style="height:60px;" name="message"></textarea>
+				    <br/>
+				    <input type="hidden" value="1" name="priority" />
+				    <input type="submit" class="button floatR clear clearWidth" name="submit" value="submit">
+				  </form>
+				</div>
 			</div>
 		</div>
 		<footer>© Copy Rights reserved - XSP Xtra Sensory Perception</footer>
